76d9f0b06ac2b12e5e5ab98cde02d35a84dcae91
sultaniyazz <sultaniyazdev@gmail.com>
2024-12-19 16:20:11 +0500

    new3

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,25 @@ import { MdPeopleAlt } from "react-icons/md";
 
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import React from 'react'
+import { useInView } from "react-intersection-observer";
 
 const Banner = ({ img, h1, p }) => {
+    const { ref: ref1, inView: inView1 } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+    const { ref: ref2, inView: inView2 } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+    const { ref: ref3, inView: inView3 } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+    const { ref: ref4, inView: inView4 } = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
     return (
         <div className='w-full h-[85vh] max-md:h-[90vh] flex justify-center items-center '>
             <div className='w-[85vw] max-md:w-[95%] max-md:p-[2vw] relative shadow-[0px_5px_10px_2px_rgba(34,60,80,0.2)] h-[80vh] max-md:h-[82vh] bg-white justify-between flex rounded-[1vw]'>
@@ -13,16 +30,16 @@ const Banner = ({ img, h1, p }) => {
                 <div className='md:absolute w-full h-full  items-center flex max-md:flex-col md:justify-between'>
                     <div className='w-[40%] max-md:w-full max-md:flex-1 md:bg-black md:h-full rounded-[1vw] opacity-60 flex justify-center text-white items-center'>
                         <div className='flex flex-col gap-[1vw] max-md:text-black w-[80%] max-md:w-full'>
-                            <h1 className='text-[3vw] max-md:text-[10vw]'>{h1}</h1>
-                            <p className='text-[1.4vw] max-md:text-[4vw]'>{p}</p>
+                            <h1 ref={ref1} className={`${inView1 ? "opacity-100 top-0" : "opacity-0 top-[5vw]"} transition-all duration-1000 relative text-[3vw] max-md:text-[10vw]`}>{h1}</h1>
+                            <p ref={ref2} className={`${inView1 ? "opacity-100 top-0" : "opacity-0 top-[6vw]"} transition-all duration-1000 relative text-[1.4vw] max-md:text-[4vw]`}>{p}</p>
                         </div>
                     </div>
                     <div className='w-[60%] max-md:w-full  max-md:flex-1  flex flex-col md:h-full gap-[1vw] items-center md:justify-center max-md:justify-start'>
-                        <div className='flex text-[10vw] max-md:text-[15vw] items-center gap-[1vw] rounded-[10vw]'>
-                            <span className=" flex items-center">+12 <p>C</p> </span>
+                        <div ref={ref3} className={`${inView3 ? "opacity-100 top-0" : "opacity-0 top-[5vw]"} transition-all duration-1000 relative flex text-[10vw] max-md:text-[15vw] items-center gap-[1vw] rounded-[10vw]`}>
+                            <span className={`flex items-center`}>+12 <p>C</p> </span>
                             <span><TiWeatherPartlySunny /></span>
                         </div>
-                        <div className="md:w-[80%] max-md:w-[90%]  justify-center max-md:shadow-md flex p-[1vw] max-md:py-[2vw] bg-white rounded-[1vw]">
+                        <div ref={ref4} className={`${inView1 ? "opacity-100 top-0" : "opacity-0 top-[2vw]"} transition-all duration-1000 relative md:w-[80%] max-md:w-[90%]  justify-center max-md:shadow-md flex p-[1vw] max-md:py-[2vw] bg-white rounded-[1vw]`}>
                             <div className="flex-1 flex justify-center items-center text-[2vw] max-md:text-[6vw] text-black">
                                 <span className="text-[3vw] max-md:text-[6vw]"><MdPeopleAlt /></span>
                                 <span>123</span>
--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -6,7 +6,9 @@ import Footer from '../components/Footer'
 
 const Destinations = () => {
     const destinations = useSelector(state => state.destinationsSlice.destinations)
-    console.log(destinations);
+    const load = useSelector(state => state.destinationsSlice.isDestLoad)
+    console.log(load);
+    const newArr = [1, 2, 3, 4]
 
     return (
         <div className='overflow-x-hidden'>
@@ -17,23 +19,46 @@ const Destinations = () => {
                     <p className='text-[2.5vw] max-md:text-[5vw]'>Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet.</p>
                 </div>
                 <div className='p-[1vw] w-full gap-[2vw] max-md:gap-[10vw] grid grid-cols-4 max-md:grid-cols-1'>
-                    {destinations.map((dest) => (
-                        <div key={dest.id} className='w-full flex flex-col  bg-red-500 justify-cente rounded-[1vw]'>
-                            <div>
-                                <img className='w-full h-[25vw] max-md:h-[100vw] object-cover rounded-t-[1vw]' src={dest.image} alt="" />
-                            </div>
-                            <div className='py-[1vw] max-md:py-[2vw] px-[2vw] justify-between h-full gap-[.8vw] max-md:gap-[3vw] flex flex-col bg-white border-b-black border-[.1vw] rounded-b-[1vw]'>
+                    {load ?
+                        newArr.map((_, index) => (
+                            <div key={index} className='w-full flex flex-col  justify-cente rounded-[1vw]'>
                                 <div>
-                                    <h1 className='text-[2vw] max-md:text-[10vw]'>{dest.country}</h1>
-                                    <p className='max-md:text-[5vw]'>{dest.description}</p>
+                                    <p className='w-full h-[25vw] bg-gray-300 animate-pulse max-md:h-[100vw] object-cover rounded-t-[1vw]' />
                                 </div>
-                                <div className='w-full flex justify-end'>
-                                    <button className='bg-orange-500 active:scale-95 max-md:text-[6vw] max-md:px-[10vw] px-[2vw] py-[.3vw] text-white font-bold rounded-full'>view</button>
+                                <div className='py-[1vw] max-md:py-[2vw] px-[2vw] justify-between h-full gap-[.8vw] max-md:gap-[3vw] flex flex-col bg-white border-b-black border-[.1vw] rounded-b-[1vw]'>
+                                    <div className='flex flex-col max-md:gap-[3vw] gap-[1vw]'>
+                                        <h1 className='text-[2vw] max-md:text-[10vw] max-md:w-[50vw] w-[10vw] bg-gray-300 max-md:h-[4vw] h-[1.2vw] rounded-full'></h1>
+                                        <h1 className='text-[1.2vw] max-md:text-[10vw] max-md:w-[80vw] w-[15vw] bg-gray-300 max-md:h-[4vw] h-[1.2vw] rounded-full'></h1>
+                                    </div>
+                                    <div className='w-full flex justify-end'>
+                                        <button className='bg-gray-300 active:scale-95 max-md:text-[6vw] max-md:px-[10vw] px-[3vw] py-[.3vw] text-white font-bold rounded-full max-md:h-[5vw] h-[1.5vw]'></button>
+                                    </div>
                                 </div>
-                            </div>
 
-                        </div>
-                    ))}
+                            </div>
+                        ))
+
+                        :
+
+                        destinations.map((dest) => (
+                            <div key={dest.id} className='w-full flex flex-col  bg-red-500 justify-cente rounded-[1vw]'>
+                                <div>
+                                    <img className='w-full h-[25vw] max-md:h-[100vw] object-cover rounded-t-[1vw]' src={dest.image} alt="" />
+                                </div>
+                                <div className='py-[1vw] max-md:py-[2vw] px-[2vw] justify-between h-full gap-[.8vw] max-md:gap-[3vw] flex flex-col bg-white border-b-black border-[.1vw] rounded-b-[1vw]'>
+                                    <div>
+                                        <h1 className='text-[2vw] max-md:text-[10vw]'>{dest.country}</h1>
+                                        <p className='max-md:text-[5vw]'>{dest.description}</p>
+                                    </div>
+                                    <div className='w-full flex justify-end'>
+                                        <button className='bg-orange-500 active:scale-95 max-md:text-[6vw] max-md:px-[10vw] px-[2vw] py-[.3vw] text-white font-bold rounded-full'>view</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        ))
+
+                    }
                 </div>
             </div>
             <div>
